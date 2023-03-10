aa401b26c9eb4b13aa3bdcfb42d32c49e8478c4c
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-10 13:13:28 +0200

    Edited Components Foldering Structure

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Layout from './components/Layout';
-import Portfolio from './components/Portfolio';
-import Dashboard from './components/Dashboard';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Layout from './components/Layout/Layout';
+import Portfolio from './components/Portfolio/Portfolio';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
